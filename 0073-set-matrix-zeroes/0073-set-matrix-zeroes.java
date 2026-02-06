73f6128d717dde6c353de4619a340669eb9a9097
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-02-06 13:58:59 +0530

    Time: 1 ms (59.92%), Space: 47.8 MB (12.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,26 +2,31 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        List<List<Integer>> list = new ArrayList<>();
+        int[][] arr = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = matrix[i][j];
+            }
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j] == 0){
-                    list.add(Arrays.asList(i,j));
+                    setMatrix(matrix,i,j,arr);
                 }
             }
         }
-        for(List<Integer> pair:list){
-            int row = pair.get(0);
-            int col = pair.get(1);
-            transform(matrix,row,col);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j] = arr[i][j];
+            }
         }
     }
-    public static void transform(int[][] matrix,int row,int col){
-       for(int i=0;i<matrix[0].length;i++){
-            matrix[row][i] = 0; 
-       }
-       for(int i=0;i<matrix.length;i++){
-            matrix[i][col] = 0;
-       }
+    public void setMatrix(int[][] matrix,int row,int col,int[][] arr){
+        for(int i=0;i<matrix[0].length;i++){
+            arr[row][i] = 0;
+        }
+        for(int i=0;i<matrix.length;i++){
+            arr[i][col] = 0;
+        }
     }
 }
