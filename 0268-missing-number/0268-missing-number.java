bcf9573d51f888d20293d217e48df68fd5f7370e
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-02-06 13:31:44 +0530

    Time: 0 ms (100%), Space: 47.3 MB (79.71%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
-        int value = n*(n+1)/2;
-        return value - sum;
+        int value = (n)*(n+1)/2;
+        return value-sum;
     }
 }
