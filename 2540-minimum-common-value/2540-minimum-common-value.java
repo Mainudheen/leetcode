aa250926267b43e59dc096c821855fb6ee44226a
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-03-10 05:29:25 +0530

    Time: 1 ms (99.13%), Space: 80.1 MB (96.2%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -10,9 +10,9 @@ class Solution {
             }else if(nums1[i]==nums2[j]){
                 return nums1[i];
             }else if(nums1[i] > nums2[j]){
-                return -1;
+                j++;;
             }
         }
-        return 0;
+        return -1;
     }
 }
