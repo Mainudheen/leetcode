865b06c058ebd46bbdd5957b65bafc03f6dad430
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-03-13 09:26:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,16 +2,19 @@ class Solution {
     public boolean isPalindrome(String s) {
         String str = "";
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)>=65 && s.charAt(i)<=90){
-                str += (char)(s.charAt(i)+32);
-            }else if((s.charAt(i)>=97 && s.charAt(i)<=122)||s.charAt(i)>='0'&& s.charAt(i)<='9'  ){
-                str+=s.charAt(i);
+            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+                str += (char) (s.charAt(i)+32);
+            }
+            else if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                str += s.charAt(i);
             }
         }
+        System.out.print(str);
+        
         int left = 0;
         int right = str.length()-1;
         while(left<right){
-            if(str.charAt(left)==str.charAt(right)){
+            if(str.charAt(left) == str.charAt(right)){
                 left++;
                 right--;
             }
@@ -19,7 +22,7 @@ class Solution {
             return false;
            }
         }
-        System.out.print(str);
         return true;
+
     }
 }
