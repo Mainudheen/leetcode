e37b5e75fc10ba43603dda532254b1471cceeb41
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-03-13 09:11:56 +0530

    Time: 6 ms (23.14%), Space: 46.1 MB (30.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,12 +4,11 @@ class Solution {
         int left = 0;
         int right = s.length()-1;
         while(left<right){
-            if(s.charAt(left)==s.charAt(right)){
-                left++;
-                right--;
-            }else{
+            if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
+            left++;
+            right--;
         }
         return true;
     }
