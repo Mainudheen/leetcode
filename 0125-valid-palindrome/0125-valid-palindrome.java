ff011658b37d362b6f3bba71dc1422ff972cd9e7
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-03-15 14:47:21 +0530

    Time: 123 ms (7.12%), Space: 48.8 MB (5.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,26 +3,23 @@ class Solution {
         String str = "";
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
-                str += (char) (s.charAt(i)+32);
-            }
-            else if(s.charAt(i)>='a' && s.charAt(i)<='z'){
-                str += s.charAt(i);
+                str+=(char)(s.charAt(i)+32);
+            }else if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                str+=s.charAt(i);
+            }else if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                str+=s.charAt(i);
             }
         }
-        System.out.print(str);
         
         int left = 0;
-        int right = str.length()-1;
+        int right= str.length()-1;
         while(left<right){
-            if(str.charAt(left) == str.charAt(right)){
-                left++;
-                right--;
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
             }
-           else{
-            return false;
-           }
+            left++;
+            right--;
         }
         return true;
-
     }
 }
