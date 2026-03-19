6ef55e9631efe9bbd8caee6734f3b2c2c976685e
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-03-19 21:27:37 +0530

    Time: 529 ms (40.25%), Space: 0B (100%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,15 @@
+select product_id, 'store1' as store,store1 as price
+from Products
+where store1 is not null
+
+union all
+
+select product_id,'store2',store2
+from products
+where store2 is not null
+
+union all
+
+select product_id,'store3',store3
+from products
+where store3 is not null;
