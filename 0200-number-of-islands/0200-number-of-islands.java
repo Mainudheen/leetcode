3b6329db9bf007d834d5509571292a344e353d94
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-04-03 08:51:23 +0530

    Time: 2 ms (99.95%), Space: 52 MB (76.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,11 +4,10 @@ class Solution {
         int n = grid[0].length;
         int count = 0;
         for(int i=0;i<m;i++){
-
             for(int j=0;j<n;j++){
                 if(grid[i][j] == '1'){
-                    count++;
                     dfs(grid,i,j);
+                    count++;
                 }
             }
         }
@@ -20,7 +19,7 @@ class Solution {
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0'){
             return;
         }
-        grid[i][j]='0';
+        grid[i][j] = '0';
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
         dfs(grid,i,j+1);
