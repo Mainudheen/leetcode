e17e89adf3a88c08b6d46e55006c905f054dd26d
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-06-07 14:28:23 +0530

    Time: 956 ms (76.79%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query state1;
-select e.name,b.bonus from Employee e
-left join Bonus b on
-e.empId = b.empId
-where b.bonus<1000 or b.bonus is null;
+# Write your MySQL query statement below
+select name,bonus
+from employee e
+left join bonus b
+on e.empId = b.empId
+where bonus < 1000 or bonus is null;
