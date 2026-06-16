29508a19febf26be42bd5addf93cd99f2d061230
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-06-16 14:07:36 +0530

    Time: 3 ms (100%), Space: 55.3 MB (48.53%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetter(s.charAt(i))){
+                sb.append(s.charAt(i));
+            }else if(s.charAt(i)=='*'){
+                if(sb.length()>=1){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }else if(s.charAt(i)=='#'){
+                sb.append(sb);
+            }else if(s.charAt(i)=='%'){
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+    }
+}
