ce45a8bffbbbb27428220173a007211e2c749d61
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-07-24 21:06:49 +0530

    Time: 77 ms (18.66%), Space: 47 MB (20.96%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s+s;
+        String sub = str.substring(1,str.length()-1);
+        return sub.contains(s);
+
+    }
+}
