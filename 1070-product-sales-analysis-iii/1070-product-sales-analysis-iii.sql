48fb552e5007e3b24e8145c15fefd6116da1b0c2
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-08-09 18:48:15 +0530

    Time: 668 ms (95.11%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+select product_id,
+        year as first_year,
+        quantity,
+        price
+from sales s1
+where (product_id,year) in (
+    select product_id,min(year)
+    from sales
+    group by product_id
+)
