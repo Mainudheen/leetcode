01db78c39cd55b3438bd0d4278e3f327788f9851
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-08-10 15:24:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (81.66%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -8,17 +8,22 @@ class Solution {
                 break;
             }
             dp[i][0] = 1;
-        }  
+        }
         for(int i=0;i<n;i++){
             if(obs[0][i] == 1){
                 break;
             }
             dp[0][i] = 1;
-        } 
+        }
+        if(obs[0][0] == 1){
+            return 0;
+        }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(obs[i][j] != 1){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                if(obs[i][j] == 1){
+                    continue;
+                }else{
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
                 }
             }
         }
