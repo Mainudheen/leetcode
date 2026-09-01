9d2071d6106a2828abc4cf2c2523a9e0a4c94b37
MAINUDHEEN S <154868756+Mainudheen@users.noreply.github.com>
2026-09-01 14:23:55 +0530

    Time: 13 ms (24.79%), Space: 51.4 MB (5.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,57 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        boolean[] arr = new boolean[strs.length];
-        List<List<String>> res = new ArrayList<>();
-        for(int i=0;i<strs.length;i++)
-        {
-            if(arr[i]==true) continue;
-            List<String> temp = new ArrayList<>();
-            String s = strs[i];
-            
-            temp.add(s);
-            arr[i] = true;
-            
-            for(int j=i+1;j<strs.length;j++)
-            {
-                if(i!=j && arr[j]!=true)
-                {
-                    String t = strs[j];
-                    int ch = check(s,t);
-                    if(ch==0)
-                    {
-                        arr[j] = true;
-                        temp.add(t);
-                    }
-                }
-                else
-                {
-                    continue;
-                }
-                
+        HashMap<List<Integer>,List<String>> map = new HashMap<>();
+        for(String s:strs){
+
+            int[] freq = new int[26];
+            for(int i=0;i<s.length();i++){
+                freq[s.charAt(i)-'a']++;
             }
-            res.add(temp);
-        }
-        return res;
-    }
-    public static int check(String s,String t)
-    {
-        int[] freq1 = new int[26];
-        
-        for(int i=0;i<s.length();i++)
-        {
-            freq1[s.charAt(i)-'a']++;
-        }
-        for(int i=0;i<t.length();i++)
-        {
-            freq1[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++)
-        {
-            if(freq1[i]!=0)
-            {
-                return 1;
+            List<Integer> key = new ArrayList<>();
+            for(int c:freq){
+                key.add(c);
             }
+            map.computeIfAbsent(key,k->new ArrayList<>()).add(s);
         }
-        return 0;
+        return new ArrayList<>(map.values());
     }
 }
